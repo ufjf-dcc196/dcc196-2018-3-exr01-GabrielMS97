340249c1374c033170dc6343f8a8742292d1276a
Gabriel Martins <gabrielmartinssantana@hotmail.com>
2018-09-12 19:44:42 -0300

    Fazendo com que o ExternoActivity retorne para o MainActivity

--- app/src/main/java/br/ufjf/dcc196/dcc196exercicio01/ExternoActivity.java
+++ app/src/main/java/br/ufjf/dcc196/dcc196exercicio01/ExternoActivity.java
@@ -1,13 +1,26 @@
 package br.ufjf.dcc196.dcc196exercicio01;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ExternoActivity extends AppCompatActivity {
+
+    private Button btnConf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_externo);
+        btnConf = (Button) findViewById(R.id.btn_confirmarExt);
+        btnConf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(Activity.RESULT_OK);
+                finish();
+            }
+        });
     }
 }
